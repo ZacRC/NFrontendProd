574e99051bffbbdf973665d98ea36ff92c342fa0
Zachary Cherney <zach@Zacharys-MacBook-Pro.local>
2024-09-03 13:56:20 -0500

    More Anylitics support

--- utils/trackPageVisit.js
+++ utils/trackPageVisit.js
@@ -1,17 +1,19 @@
 export const trackPageVisit = async (pageName) => {
   const access = localStorage.getItem('access');
-  if (access) {
-    try {
-      await fetch('https://creatorgiveaways.world/api/track_activity/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${access}`,
-        },
-        body: JSON.stringify({ activity_type: 'page_visit', details: { page: pageName } }),
-      });
-    } catch (error) {
-      console.error('Error tracking page visit:', error);
-    }
+  try {
+    await fetch('https://creatorgiveaways.world/api/track_activity/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        ...(access ? { 'Authorization': `Bearer ${access}` } : {}),
+      },
+      body: JSON.stringify({ 
+        activity_type: 'page_visit', 
+        details: { page: pageName },
+        anonymous: !access
+      }),
+    });
+  } catch (error) {
+    console.error('Error tracking page visit:', error);
   }
 };
